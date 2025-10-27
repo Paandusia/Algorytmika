047b25ff1ebd6c2b81af2801bd88ee5ab2c24724
Paandusia <asuna_3@wp.pl>
2025-10-27 10:08:18 +0100

    Wyszukiwanie

---
+++ Wyznaczanie liczb pierwszych/Wyznacznie_liczb_pierwszych.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int wyznaczanie{
+	int n;
+	
+	if (n < 2){
+		cout << n "nie jest liczba pierwsza" << endl; 
+	}
+	
+	while (d == 2 || sqrt(n)){
+		
+	}
+	
+	while (n < 2){
+		if (n % ){
+			
+		}
+	}
+	
+	return ;
+}
+
+
+int main(){
+	
+	cout << "Podaj liczbe: " << endl;
+	cin >> n
+	
+	cout << n << "jest liczba pierwsza" << endl;
+	
+	return 0;
+}
